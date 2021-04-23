907487cc4a4249a229ea2d18067a8ade5590b256
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-04-23 12:49:03 -0400

    editing the landing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import photo from './photo-Bohdan.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={photo} className="App-logo" alt="logo" />
+        <h1>
+          Sun flares on Cloudflare are particularly strong today!
+        </h1>
       </header>
     </div>
   );
