c2c1abb227b65c1d8280d5102117dfbef8ed92d4
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-04-23 13:40:47 -0400

    Updated the landing page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={photo} className="App-logo" alt="logo" />
-        <h1>
-          Sun flares on Cloudflare are particularly strong today!
-        </h1>
+        <h1>Веселих Свят! З Великоднем вас! <br /> Happy Easter!</h1>
+        <div className="anima">
+          <img src={photo} className="App-logo" alt="logo" />
+        </div>
+        <h2>
+          Cонячні відблиски на ХмароБлиску cьогодні якісь особливі! 
+          <br />
+          The Sun flares on Cloudflare are particularly strong today!
+        </h2>
       </header>
     </div>
   );
